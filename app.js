8264574d53eae5fa2e21ece9290d9d949fdca729
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-05-03 10:37:09 +0530

    work on UI

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//db
+
+const db = require('./models/connect')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
