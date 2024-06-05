5dd7d9bb1f40bfc702abf72e1a479c1d0cc53bc1
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-06-05 11:47:15 +0530

    online db connected

--- models/connect.js
+++ models/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/social_media")
+mongoose.connect("mongodb+srv://bharat24me025:bharat123@cluster0.b2mtn45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then( ()=> console.log("db connected"))
     .catch( (err) => console.log(err))
